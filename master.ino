45a393b06ebe4130c3bbb6884829227b2a66f074
neonowy <chip0012@gmail.com>
2014-03-30 00:02:55 +0100

    Cleaning comments and importing new libraries.

--- master.ino
+++ master.ino
@@ -1,14 +1,4 @@
 /*
-  LiquidCrystal Library - Blink
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD and makes the 
- cursor block blink.
- 
  The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -20,29 +10,14 @@
  * 10K resistor:
    * ends to +5V and ground
    * wiper to LCD VO pin (pin 3)
- 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe 
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
+  */
 
- http://arduino.cc/en/Tutorial/LiquidCrystalBlink
- 
- */
-
-#include <Wire.h> // necessary, or the application won't build properly
-#include <stdio.h>
+#include <Wire.h>
 #include <PCF8583.h>
 #include <LiquidCrystal.h>
+#include "OneWire.h"
+#include "DallasTemperature.h"
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int correct_address = 0;
 PCF8583 p (0xA0);
 int lLvl;
