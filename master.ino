9008dc14fffda96b8ce355eaaca9dca46c16af18
neonowy <_neonn@outlook.com>
2014-07-01 03:04:23 +0200

    Reorganized things into functions and many improvments.

--- master.ino
+++ master.ino
@@ -1,80 +1,177 @@
-/*
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
-   * ends to +5V and ground
-   * wiper to LCD VO pin (pin 3)
-  */
-
 #include <Wire.h>
 #include <PCF8583.h>
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <studio.h>
 
 #define ONE_WIRE_BUS 7
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int correct_address = 0;
+LiquidCrystal lcd(12, 6, 5, 4, 3, 2);
 PCF8583 p (0xA0);
-int lLvl;
-int buttPin = 8;
-int backlightPin = 9;
+int ButtPin = 8;
+int BacklightPin = 9;
+int LDRpin = A0;
+
+byte bell[8] = {
+        B00100,
+        B01110,
+        B01110,
+        B11111,
+        B11111,
+        B00100,
+        B00000
+     };
+
+byte degree[8] = {
+        B00111,
+        B00101,
+        B00111,
+        B00000,
+        B00000,
+        B00000,
+        B00000
+     };
+
+void greetings() {
+    char powitanie[] =  "Witaj Bartek !";
+    lcd.setCursor(0,1);
+    lcd.print(powitanie);
+    delay(1500);
+    lcd.clear();
+    
+    for(int i=0;i<=1;i++) { 
+        lcd.setCursor(i,1);
+        lcd.print(powitanie);
+        delay(250);
+        lcd.clear();
+    
+        if(i == 1) {
+            int x = i+1;
+            for(int j = 14;j>0;j--) {
+                lcd.setCursor(x,1);
+                //lcd.print(powitanie.substring(0,j));
+                for(int z = 0;z<j;z++) {
+                    lcd.print(powitanie[z]);
+                }
+                delay(250);
+                lcd.clear();
+                x++;
+            }
+        }
+    }
+}
+
+// getDate function
+// Returns date in char array.
+// Parameters: object of PCF8583 class
+// Return: date in char array (DD/MM/YY)
+char* getDate(PCF8583 p) {
+    p.get_time();
+    char date[50];
+    sprintf(date, "%02d/%02d/%02d",
+    p.day, p.month, p.year);
+    return(date);
+}
+
+// getDayOfWeek function
+// Returns day of week in char array.
+// Parameters: object of PCF8583 class
+// Return: Day of week in char array
+char* getDayOfWeek(PCF8583 p) {
+    p.get_time();
+    int dayOfWeek = p.day;
+    switch(dayOfWeek) {
+    case 1:
+        return "Poniedzialek";
+        break;
+    case 2:
+        return "Wtorek";
+        break;
+    case 3:
+        return "Sroda";
+        break;
+    case 4:
+        return "Czwartek";
+        break;
+    case 5:
+        return "Piatek";
+        break;
+    case 6:
+        return "Sobota";
+        break;
+    case 7:
+        return "Niedziela";
+        break;
+    default:
+        return "Error";
+    }
+}
+// getTime function
+// Returns time in char array.
+// Parameters: object of PCF8583 class
+// Return: time in char array (HH:MM:SS)
+char* getTime(PCF8583 p) {
+    p.get_time();
+    char time[50];
+    sprintf(time, "%02d:%02d:%02d",
+    p.hour, p.minute, p.second);
+    return(time);
+}
+
+// setBacklight function
+// Gets light level form LDR (via ADC) and set LCD backligt level (via PWM).
+// Parameters: LDR pin
+// Return: none
+void setBacklight(int LDRpin, int ButtPin, int BacklightPin, int darkLvl, int mediumLvl) {
+    int lLvl = map(analogRead(LDRpin), 0, 1023, 0, 255);
+    int backlightLvl;
+    if (lLvl > darkLvl) backlightLvl = 1;
+    else if (lLvl > mediumLvl) backlightLvl = 150;
+    else backlightLvl = 255;
+    if (digitalRead(ButtPin) == LOW) digitalWrite(BacklightPin, HIGH);
+    else analogWrite(BacklightPin, backlightLvl);
+}
+
+void printHome() {
+    lcd.setCursor(0,0);
+    lcd.print(getDate(p));
+    lcd.setCursor(0,1);
+    lcd.print(getTime(p));
+    lcd.setCursor(-4,2);
+    lcd.print(getDayOfWeek(p));
+    lcd.setCursor(15,0);
+    lcd.write(byte(0));
+    lcd.setCursor(-4,3);
+    sensors.requestTemperatures(); 
+    lcd.print(sensors.getTempCByIndex(0));
+    lcd.setCursor(0,3);
+    lcd.write(byte(1));
+    
+}
+
 void setup() {
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 4);
-  // Print a message to the LCD.
-  pinMode(buttPin, INPUT_PULLUP);
-  pinMode(backlightPin, OUTPUT);
-  digitalWrite(backlightPin, HIGH);
+  pinMode(ButtPin, INPUT_PULLUP);
+  pinMode(BacklightPin, OUTPUT);
+  pinMode(LDRpin, INPUT);
+  digitalWrite(BacklightPin, HIGH);
+  sensors.begin(); 
+     
+  lcd.createChar(0, bell);  
+  lcd.createChar(1, degree);        
+        
   Serial.begin(9600);
   sensors.begin();
+  greetings();
+  lcd.clear();
 }
 
 void loop() {
-  p.get_time();
-  sensors.requestTemperatures();
-  lLvl = map(analogRead(A0), 0, 1023, 0, 255);
-
-  if (digitalRead(buttPin) == LOW && lLvl <= 240) {
-    digitalWrite(9, HIGH);
-    char time[50];
-    for (int i=0;i<6;i++) {
-      p.get_time();
-      sensors.requestTemperatures();
-
-      sprintf(time, "%02d:%02d:%02d",
-      p.hour, p.minute, p.second);
-      lcd.setCursor(0,0);
-      lcd.print(time);
-      lcd.setCursor(-4,2);
-      lcd.print(sensors.getTempCByIndex(0));
-      delay(1000);
-      lcd.clear();
-    }
-    analogWrite(9, 2);
-  }
-  else {
-  if (lLvl > 240) analogWrite(9, 1);
-  else if (lLvl > 210) analogWrite(9, 150);
-  else analogWrite(9, 255);
-  }
-  char time[50];
-  sprintf(time, "%02d:%02d:%02d",
-	  p.hour, p.minute, p.second);
-  lcd.print(time);
-  // Turn off the blinking cursor:
-   // Turn on the blinking cursor:
-  delay(1000);
-  Serial.println(lLvl);
-  lcd.clear();  
+    setBacklight(LDRpin, ButtPin, BacklightPin, 240, 210);
+    printHome();
+    delay(1000);
 }
-
-
