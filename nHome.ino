eac94be974fe4cfb09c4fe82661b2e7d41541cee
neonowy <neonowy@users.noreply.github.com>
2014-07-01 20:19:21 +0200

    ...

--- nHome.ino
+++ nHome.ino
@@ -1,23 +1,28 @@
 #include <Wire.h>
 #include <PCF8583.h>
+#include <studio.h>
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <studio.h>
 #include <VirtualWire.h>
 
+
 #define ONE_WIRE_BUS 7
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t bufLen = VW_MAX_MESSAGE_LEN;
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 int correct_address = 0;
-LiquidCrystal lcd(12, 6, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 6, 5, 4, 10, 2);
 PCF8583 p (0xA0);
 int ButtPin = 8;
-int BacklightPin = 9;
+int BuzzPin = 13;
+int BacklightPin = 3;
 int LDRpin = A0;
-bool letters = 0;
-uint8_t buf[VW_MAX_MESSAGE_LEN];
-uint8_t bufLen = VW_MAX_MESSAGE_LEN;
+int letters = 0;
+int beepsCounter = 0;
+String message;
 byte bell[8] = {
         B00100,
         B01110,
@@ -85,8 +90,7 @@ char* getDate(PCF8583 p) {
 // Return: Day of week in char array
 char* getDayOfWeek(PCF8583 p) {
     p.get_time();
-    int dayOfWeek = p.day;
-    //Serial.println(dayOfWeek);
+    int dayOfWeek = p.get_day_of_week();
     switch(dayOfWeek) {
     case 1:
         return "Poniedzialek";
@@ -146,15 +150,8 @@ void printHome() {
     lcd.print(getTime(p));
     lcd.setCursor(-4,2);
     lcd.print(getDayOfWeek(p));
-    if (letters > 9) {
-    	lcd.setCursor(13, 0);
-    	lcd.print(letters);
-    	lcd.write(byte(0));
-    } else {
-    	lcd.setCursor(14,0);
-    	lcd.print(letters);
-    	lcd.write(byte(0));
-    }
+    lcd.setCursor(15,0);
+    reloadBell();
     lcd.setCursor(-4,3);
     sensors.requestTemperatures(); 
     lcd.print(sensors.getTempCByIndex(0));
@@ -163,40 +160,60 @@ void printHome() {
     lcd.print("C");
 }
 
-void receiveLetters() {
-	if (vw_get_message(buf, &bufLen)) {
-		letters = (bool) buf[0];
-		Serial.println("foo!");
-		Serial.println(buf[0]);
-	}
-	Serial.println("bar!");
+void receive() {
+    if (vw_get_message(buf, &bufLen)) {
+        message = "";
+        for(int i = 0; i < bufLen; i++) {
+            message += char(buf[i]);
+        }
+        
+    }
+}
+
+void reloadBell() {
+    if (message == "lette" && beepsCounter < 3 || message == "letter" && beepsCounter < 3) {
+        lcd.write(byte(0));
+        Serial.println("foo!");
+        digitalWrite(BuzzPin, HIGH);
+        delay(500);
+        digitalWrite(BuzzPin, LOW);
+        beepsCounter++;
+        Serial.println(beepsCounter);
+    } else if (message == "empty") {
+        lcd.print(" ");
+        digitalWrite(BuzzPin, LOW);
+        beepsCounter = 0;
+    } else {
+        digitalWrite(BuzzPin, LOW);
+    }
 }
 
 void setup() {
-	vw_set_ptt_inverted(true);
-	vw_setup(2000);
-	vw_rx_start();
-  	// set up the LCD's number of columns and rows:
-  	lcd.begin(16, 4);
-  	pinMode(ButtPin, INPUT_PULLUP);
-  	pinMode(BacklightPin, OUTPUT);
-  	pinMode(LDRpin, INPUT);
-  	digitalWrite(BacklightPin, HIGH);
-  	sensors.begin(); 
-  	sensors.setResolution(9);
-     
-  	lcd.createChar(0, bell);  
-  	lcd.createChar(1, degree);        
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(2000);  // Bits per sec
+    vw_rx_start();
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 4);
+    pinMode(ButtPin, INPUT_PULLUP);
+    pinMode(BacklightPin, OUTPUT);
+    pinMode(LDRpin, INPUT);
+    pinMode(BuzzPin, OUTPUT);
+    digitalWrite(BacklightPin, HIGH);
+    sensors.begin(); 
+    sensors.setResolution(9);  
+    lcd.createChar(0, bell);  
+    lcd.createChar(1, degree);        
         
-  	Serial.begin(9600);
-  	sensors.begin();
-  	greetings();
-  	lcd.clear();
+    Serial.begin(9600);
+    sensors.begin();
+    greetings();
+    lcd.clear();
 }
 
 void loop() {
     setBacklight(LDRpin, ButtPin, BacklightPin, 240, 210);
     printHome();
-    receiveLetters();
-    delay(1000);
+    receive();
+    Serial.print(message);
+    delay(500);
 }
